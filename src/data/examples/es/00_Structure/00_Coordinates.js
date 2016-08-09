429eee76e64bf425a93d963ddfe35e75a2cbc36f
aaron <aamontoya@gmail.com>
2016-08-09 12:40:16 -0400

    translation to spanish of structure examples

--- src/data/examples/es/00_Structure/00_Coordinates.js
+++ src/data/examples/es/00_Structure/00_Coordinates.js
@@ -1,40 +1,39 @@
 /*
  * @name Coordenadas
- * @description Todas las formas... drawn to the screen have a position that is 
- * specified as a coordinate. All coordinates are measured as the distance from
- * the origin in units of pixels. The origin [0, 0] is the coordinate in the
- * upper left of the window and the coordinate in the lower right is [width-1, 
- * height-1].
+ * @description Todas las formas dibujadas en la pantalla tienen una posición que es
+ * especificada como una coordenada. Todas las coordenadas son medidas como una distancia desde el origen, usando el pixel como unidad de medida.
+ * El origen [0, 0] es la coordenada en la esquina superior izquierda de la ventana y la coordenada de la esquina inferior derecha es [ancho-1, altura-1].
  */
 function setup() {
-  // @TODO en español
-  // Sets the screen to be 640 pixels wide and 360 pixels high
+
+  // Definir lienzo de 720 pixeles de ancho y 400 pixeles de alto
   createCanvas(720, 400);
 }
 
 function draw() {
-  // Set the background to black and turn off the fill color
+  // Definir el color del fondo como negro
+  // y definir que las figuras sean pintadas sin relleno
   background(0);
   noFill();
 
-  // The two parameters of the point() method each specify 
-  // coordinates.
-  // The first parameter is the x-coordinate and the second is the Y 
+  // Los dos parámetros del método point() especifican coordenadas.
+  // El primer parámetro es la coordenada x y el segundo es la y.
   stroke(255);
   point(width * 0.5, height * 0.5);
-  point(width * 0.5, height * 0.25); 
+  point(width * 0.5, height * 0.25);
 
-  // Coordinates are used for drawing all shapes, not just points.
-  // Parameters for different functions are used for different 
-  // purposes. For example, the first two parameters to line()  
-  // specify the coordinates of the first endpoint and the second  
-  // two parameters specify the second endpoint
+  // Las coordenadas se usan para dibujar figuras, no solo puntos.
+  // Los parámetros de las funciones son usados para múltiples
+  // propósitos. Por ejemplo, los dos primeros parámetros
+  // de line() especifican las coordenadas del primer extremo
+  //y los siguientes dos parámetros del segundo extremo.
   stroke(0, 153, 255);
   line(0, height*0.33, width, height*0.33);
 
-  // By default, the first two parameters to rect() are the 
-  // coordinates of the upper-left corner and the second pair
-  // is the width and height
+
+  // Por defecto, los dos primeros parámetros de rect() son
+  // las coordenadas de la esquina superior izquierda y el
+  // segundo par son el ancho y el alto.
   stroke(255, 153, 0);
   rect(width*0.25, height*0.1, width * 0.5, height * 0.8);
 }
--- src/data/examples/es/00_Structure/03_No_Loop.js
+++ src/data/examples/es/00_Structure/03_No_Loop.js
@@ -1,30 +1,30 @@
 /*
  * @name No Loop
- * @description The noLoop() function causes draw() to only execute once. 
- * Without calling noLoop(), the code inside draw() is run continually.
+ * @description La función noLoop() hace que draw() se ejecuta solo una vez.
+ * Sin ejecutar noLoop(), el código dentro de draw() es ejecutado continuamente.
  */
 var y;
 
-// The statements in the setup() function 
+// The statements in the setup() function
 // execute once when the program begins
-function setup() 
+function setup()
 {
   // createCanvas should be the first statement
-  createCanvas(720, 400);  
+  createCanvas(720, 400);
   stroke(255);     // Set line drawing color to white
   noLoop();
-  
+
   y = height * 0.5;
 }
 
-// The statements in draw() are executed until the 
-// program is stopped. Each statement is executed in 
-// sequence and after the last line is read, the first 
+// The statements in draw() are executed until the
+// program is stopped. Each statement is executed in
+// sequence and after the last line is read, the first
 // line is executed again.
-function draw() 
-{ 
+function draw()
+{
   background(0);   // Set the background to black
-  y = y - 1; 
-  if (y < 0) { y = height; } 
-  line(0, y, width, y);  
-} 
+  y = y - 1;
+  if (y < 0) { y = height; }
+  line(0, y, width, y);
+}
