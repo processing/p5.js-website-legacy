4a2f556fc87a7726a3ba39ac502dd44a02c71048
aaron <aamontoya@gmail.com>
2016-08-16 22:07:54 -0400

    add spanish translation of sound examples

--- src/data/examples/es/33_Sound/21_FreqModulation.js
+++ src/data/examples/es/33_Sound/21_FreqModulation.js
@@ -1,68 +1,67 @@
 /**
  * @name Modulación en frecuencia
- * @description <p> La modulación en frecuencia es una poderosa forma de síntesis.
- * In its simplest form, FM involves two oscillators, referred
- * to as the carrier and the modulator. As the modulator's waveform oscillates
- * between some minimum and maximum amplitude value, that momentary value
- * is added to ("modulates") the frequency of the carrier.</p>
- * <p>The <b>carrier</b> is typically set to oscillate at an audible frequency
- * that we perceive as a pitch—in this case, it is a sine wave oscilaltor at 220Hz,
- * equivalent to an "A3" note. The carrier is connected to master output by default
- * (this is the case for all p5.Oscillators).</p>
- * <p>We will <code>disconnect</code> the <b>modulator</b> from master output,
- * and instead connect to the frequency of the carrier:
- * <code>carrier.freq(modulator)</code>. This adds the output amplitude of the
- * modulator to the frequency of the carrier.</p>
+ * @description <p> La modulación en frecuencia (FM) es una poderosa forma de síntesis.
+ * En su forma más simple , la FM involucra dos osciladores, referidos
+ * como la portadora y la modulante. Mientras la onda modulante oscila
+ * entre algún valor mínimo y maximo de amplitud, el valor momentáneo
+ * es sumado a ("modula") la frecuencia de la portadora.</p>
+ * <p>La <b>portadora</b> es típicamente definida para oscilar a una frecuencia audible
+ * que percibimos como una altura — en este caso, es un oscilador sinusoidal a 220 Hz,
+ * equivalenta a una nota "A3". La portadora es conectada a la salida maestra por defecto
+ * (este es el caso para todos los objetos p5.Oscillator).</p>
+ * <p>Nosotros <code>desconectaremos</code> la <b>modulante</b> de la salida maestra,
+ * y la conectaremos a la frecuencia de la portadora:
+ * <code>carrier.freq(modulator)</code>. Esto suma la amplitud de salida de la moduante
+ * a la frecuencia de la portadora.</p>
  * <p>
- * <b>Modulation Depth</b> describes how much the carrier frequency will modulate.
- * It is based on the amplitude of the modulator.
- * The modulator produces a continuous stream of amplitude values that we will add
- * to the carrier frequency. An amplitude of zero means silence, so the modulation will
- * have no effect. An amplitude of 1.0 scales the range of output values
- * between +1.0 and -1.0. That is the standard range for sound that gets sent to
- * your speakers, but in FM we are instead sending the modulator's output to the carrier frequency,
- * where we'd barely notice the +1Hz / -1Hz modulation.
- * So we will typically increase the amplitude ("depth") of the modulator to numbers much higher than what
- * we might send to our speakers.</p>
- * <p><b>Modulation Frequency</b> is the speed of modulation. When the modulation frequency is lower
- * than 20Hz, we stop hearing its frequency as pitch, and start to hear it as a beating rhythm.
- * For example, try 7.5Hz at a depth of 20 to mimic the "vibrato" effect of an operatic vocalist.
- * The term for this is Low Frequency Oscillator, or LFO. Modulators set to higher frequencies can
- * also produce interesting effects, especially when the frequency has a harmonic relationship
- * to the carrier signal. For example, listen to what happens when the modulator's frequency is
- * half or twice that of the carrier. This is the basis for FM Synthesis, developed by John Chowning
- * in the 1960s, which came to revolutionize synthesis in the 1980s and is often used to synthesize
- * brass and bell-like sounds.
+ * La <b>profundida de modulación</b> describe cúanto será modulada la frecuencia de la portadora.
+ * Está basada en la amplitud de la modulante.
+ * La modulante produce un torrente continuo de valores de amplitud que sumaremos
+ * a la frecuencia de la portadora. Una amplitud nula (0) significa silencio, así que la modulación no tendría efecto.
+ * Una amplitud de 1.0 escala el rango de los valores de salida
+ * entre +1.0 y -1.0. Este es el rango estándar de sonido que es enviado a tus parlantes,
+ * pero en FM estamos enviando la salida de la modulante a la frecuencia de la portadora,
+ * donde casi no notaríamosla modulación +1Hz / -1Hz.
+ * Es por eso que típicamente aumentamos la amplitud (profundidad) de la modulante a números a muchos más altos
+ * que los que enviaríamos a nuestros parlantes.</p>
+ * <p>La <b>frecuencia de modulación</b> es la velocidad de la modulación. Cuando la frecuencia de modulación es menor a
+ * 20Hz, dejamos de escuchar su frecuencia como una altura, y empezamos a escucharla como un ritmo pulsante.
+ * Por ejemplo, prueba 7.5Hz a una profundidad de 20 para imitar el efecto "vibrato" de un vocalista de ópera.
+ * El térmio par esto es un oscilador de baja frecuencia (LFO, Low Frequency Oscillator). Las modulantes configuradas a frecuencias más altas
+ * pueden también producir efectos interesantes, especialmente cuando la frecuencia tiene una relación harmónica
+ * con la señal portadora. Por ejemplo, escucha lo que sucede cuando la frecuencia modulante es
+ * la mitad o el doble de la portadora. Esto es la base de la síntesis FM, desarrollada por John Chowning
+ * en los años 1960s, lo que revolucionó la síntesis en los años 1980s y es a menudo utilizada para sintesizar sonidos de instrumentos metálicos de viento y campanas.
  *
- * <p>In this example,</p><p>
- * - MouseX controls the modulation depth (the amplitude of the modulator) from -150 to 150.
- * When the modulator's amplitude is set to 0 (in the middle), notice how the modulation
- * has no effect. The greater (the absolute value of) the number, the greater the effect.
- * If the modulator waveform is symetrical like a square <code>[]</code>, sine <code>~</code>
- * or triangle <code>/\</code>, the negative amplitude will be the same as positive amplitude.
- * But in this example, the modulator is an asymetrical sawtooth wave, shaped like this /.
- * When we multiply it by a negative number, it goes backwards like this \. To best
- * observe the difference, try lowering the frequency.
+ * <p>En este ejemplo,</p><p>
+ * - la posición horizontal del ratón (mouseX) controla la profundidad de la modulación (la amplituda de la modulante) entre -150 y 150.
+ * Cuando la amplitud de la modulante es anulada (0) en la mitad del lienzo, date cuenta cómo la modulación
+ * no tiene efecto. A mayor (aplicando valor absoluto) el número, mayor el efecto.
+ * Si la forma de onda de la modulante es simétrica como una onda cuadrada <code>[]</code>, sinusoidal <code>~</code>
+ * o triangular <code>/\</code>, la amplitud negativa será la misma que la amplitud positiva.
+ * Pero en este ejemplo, la modulante es una onda diente de sierra asimétrica, con una forma así /.
+ * Cuando la multiplicamos por un número negativo, se invierte así \.
+ * Para observar mejor la diferencia, baja la frecuencia.
  * </p>
- * <p>- MouseY controls the frequency of the modulator from 0 to 112 Hz.
- * Try comparing modulation frequencies below the audible range (which starts around 20hz),
- * and above it, especially in a harmonic relationship to the carrier frequency (which is 220hz, so
- * try half that, 1/3, 1/4 etc...).
+ * <p>- La posición vertical del ratón (mouseY) controla la frecuencia de la modulante entre 0 y 112 Hz.
+ * Prueba comparando frecuencias de modulación bajo el rango audible (que empieza en torno a los 20 Hz),
+ * y sobre él, especialmente en relación armónica a la frecuencia de la portadora (que es de 220hz, así que
+ * prueba con la mitad, 1/3, 1/4 etc...).
  *
- * <p><em><span class="small">You will need to include the
- * <a href="http://p5js.org/reference/#/libraries/p5.sound">p5.sound library</a>
- * for this example to work in your own project.</em></span></p>
+ * <p><em><span class="small">Necesitarás incluir la
+ * <a href="http://p5js.org/reference/#/libraries/p5.sound">biblioteca p5.sound</a>
+ * para que este ejemplo funcione en tu proyecto propio.</em></span></p>
  */
 
-var carrier; // this is the oscillator we will hear
-var modulator; // this oscillator will modulate the frequency of the carrier
+var carrier; // este es el oscilador que escucharemos, la portadora
+var modulator; // este oscilador modulará la frecuencia de la portadora
 
-var analyzer; // we'll use this visualize the waveform
+var analyzer; // lo usaremos para visualizar la forma de onda
 
-// the carrier frequency pre-modulation
+// la frecuencia de la portadora antes de ser modulada
 var carrierBaseFreq = 220;
 
-// min/max ranges for modulator
+// rangos mínimo y máximo para la modulante
 var modMaxFreq = 112;
 var modMinFreq = 0;
 var modMaxDepth = 150;
@@ -73,42 +72,42 @@ function setup() {
   noFill();
 
   carrier = new p5.Oscillator('sine');
-  carrier.amp(0); // set amplitude
-  carrier.freq(carrierBaseFreq); // set frequency
-  carrier.start(); // start oscillating
+  carrier.amp(0); // definir la amplitud
+  carrier.freq(carrierBaseFreq); // definir la frecuencia
+  carrier.start(); // empezar a oscilar
 
-  // try changing the type to 'square', 'sine' or 'triangle'
+  // prueba a cambiar el tipo a 'square', 'sine' or 'triangle'
   modulator = new p5.Oscillator('sawtooth');
   modulator.start();
 
-  // add the modulator's output to modulate the carrier's frequency
+  // suma la salida de la modulante para modular la frecuencia de la portadora
   modulator.disconnect();
   carrier.freq( modulator );
 
-  // create an FFT to analyze the audio
+  // crea una FFT para analizar el audio
   analyzer = new p5.FFT();
 
-  // fade carrier in/out on mouseover / touch start
+  // prende o apaga la portadora según si el ratón está sobre el botón de inicio o lo presiona
   toggleAudio(cnv);
 }
 
 function draw() {
   background(30);
 
-  // map mouseY to modulator freq between a maximum and minimum frequency
+  // mapea la posición vertical del ratón (mouseY) a la frecuencia modulante entre las frecuencias mínima y máxima
   var modFreq = map(mouseY, height, 0, modMinFreq, modMaxFreq);
   modulator.freq(modFreq);
 
-  // change the amplitude of the modulator
-  // negative amp reverses the sawtooth waveform, and sounds percussive
+  // cambia la amplitud de la modulante
+  // la amplitud negativa pone en reversa la forma de onda diente de sierra, suena percusiva
   //
   var modDepth = map(mouseX, 0, width, modMinDepth, modMaxDepth);
   modulator.amp(modDepth);
 
-  // analyze the waveform
+  // analiza la forma de onda
   waveform = analyzer.waveform();
 
-  // draw the shape of the waveform
+  // dibuja la forma de onda
   stroke(255);
   strokeWeight(10);
   beginShape();
@@ -120,14 +119,14 @@ function draw() {
   endShape();
 
   strokeWeight(1);
-  // add a note about what's happening
-  text('Modulator Frequency: ' + modFreq.toFixed(3) + ' Hz', 20, 20);
-  text('Modulator Amplitude (Modulation Depth): ' + modDepth.toFixed(3), 20, 40);
-  text('Carrier Frequency (pre-modulation): ' + carrierBaseFreq + ' Hz', width/2, 20);
+  // añade una nota sobre lo que está pasando
+  text('Frecuencia modulante: ' + modFreq.toFixed(3) + ' Hz', 20, 20);
+  text('Amplitud modulante (profundidad de modulación): ' + modDepth.toFixed(3), 20, 40);
+  text('Frecuencia portadora (antes de la modulación): ' + carrierBaseFreq + ' Hz', width/2, 20);
 
 }
 
-// helper function to toggle sound
+// función de ayuda para prender y apagar el sonido
 function toggleAudio(cnv) {
   cnv.mouseOver(function() {
     carrier.amp(1.0, 0.01);
