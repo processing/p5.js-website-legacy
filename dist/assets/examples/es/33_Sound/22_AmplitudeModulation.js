f0824bec744c518bbf0ca7720733cc905f232127
aaron <aamontoya@gmail.com>
2016-08-16 22:22:02 -0400

    update with grunt server

--- dist/assets/examples/es/33_Sound/22_AmplitudeModulation.js
+++ dist/assets/examples/es/33_Sound/22_AmplitudeModulation.js
@@ -1,35 +1,35 @@
 /**
- * @name Amplitude Modulation
- * @description <p>Amplitude Modulation involves two oscillators, referred
- * to as the carrier and the modulator, where the modulator controls
- * the carrier's amplitude.</p>
+ * @name Modulación en amplitud
+ * @description <p>La modulación en amplitud involucra dos osciladores, referidos como
+ * la portadora (carrier) y la modulante, donde la modulante controla la amplitud de la portadora.</p>
  *
- * <p>The carrier is typically set at an audible frequency (i.e. 440 Hz)
- * and connected to master output by default. The carrier.amp is
- * set to zero because we will have the modulator control its amplitude.</p>
+ * <p>La portadora es típicamente definida a una frecuencia audible (por ejemplo, 440 Hz)
+ * y conectada a la salida maestra por defecto. La amplitud de la portadora (carrier.amp) es anulada (0)
+ * porque así la modulante tiene total control sobre su amplitud.</p>
  *
- * <p>The modulator is disconnected from master output. Instead, it is connected
- * to the amplitude of the Carrier, like this: carrier.amp(modulator).</p>
+ * <p>La modulante es desconectada de la salida maestra. En vez de eso, es conectada
+ * a la amplitud de la portadora, así: carrier.amp(modulator).</p>
  *
- * <p>In this example...</p>
- * <p>- MouseX controls the amplitude of the modulator
- * from 0 to 1. When the modulator's amplitude is set to 0, the
- * amplitude modulation has no effect.</p>
+ * <p>En este ejemplo:</p>
+ * <p>- la posición horizontal del ratón (mouseX) contrla la amplitud de la modulante
+ * entre 0 y 1. Cuando la amplitud modulante es hecha 0, la
+ * modulación en amplitud no hace efecto.</p>
  *
- * <p>- MouseY controls the frequency of the modulator from 0 to 20hz.
- * This range is lower frequencies than humans can hear, and we perceive the
- * modulation as a rhythm. This range can simulate effects such as Tremolo.
- * Ring Modulation is a type of Amplitude Modulation where the original
- * carrier signal is not present, and often involves modulation at a faster
- * frequency. </p>
+ * <p>- La posición vertical del ratón (mouseY) controla la frecuencia de la modulante entre 0 y 20 Hz.
+ * Este rango es más grave que las frecuencias que los humanos pueden escuchar, y percibimos la
+ * modulación como un ritmo. Este rango simula un efecto conocido como tremolo.
+ * La modulación en anillo (ring modulation) es un tipo de modulación en amplitud donde la
+ * señal portadora original no está presente, y a menudo involucra modulación a una mayor
+ * frecuencia. </p>
  *
- * <p><em><span class="small">You will need to include the
- * <a href="http://p5js.org/reference/#/libraries/p5.sound">p5.sound library</a>
- * for this example to work in your own project.</em></span></p>
+ * <p><em><span class="small">Necesitarás incluir la
+ * <a href="http://p5js.org/reference/#/libraries/p5.sound">biblioteca p5.sound</a>
+ * para que este ejemplo funcione en tu proyecto propio.</em></span></p>
  */
-var carrier; // this is the oscillator we will hear
-var modulator; // this oscillator will modulate the amplitude of the carrier
-var fft; // we'll visualize the waveform
+ 
+var carrier; // este es el oscilador que escucharemos
+var modulator; // este oscilador modulará la amplitud de la portadora
+var fft; // visualizaremos la forma de onda
 
 function setup() {
   createCanvas(800,400);
@@ -37,41 +37,41 @@ function setup() {
   background(30); // alpha
 
 
-  carrier = new p5.Oscillator(); // connects to master output by default
+  carrier = new p5.Oscillator(); // se conecta la salida a la salida maestra por defecto
   carrier.freq(340);
   carrier.amp(0);
-  // carrier's amp is 0 by default, giving our modulator total control
+  // la amplitud de la portadora es 0 por defecto, dándole así a a la modulante control total
 
   carrier.start();
 
   modulator = new p5.Oscillator('triangle');
-  modulator.disconnect();  // disconnect the modulator from master output
+  modulator.disconnect();  // desconecta la moduladora de la salida maestra
   modulator.freq(5);
   modulator.amp(1);
   modulator.start();
 
-  // Modulate the carrier's amplitude with the modulator
-  // Optionally, we can scale the signal.
+  // modula la ampltiud de la portadora con la modulante
+  // opcionalmente, podemos escalar la señal
   carrier.amp(modulator.scale(-1,1,1,-1));
 
-  // create an fft to analyze the audio
+  // crea una FFT para analizar el audio
   fft = new p5.FFT();
 }
 
 function draw() {
   background(30,30,30,100); // alpha
 
-  // map mouseY to moodulator freq between 0 and 20hz
+  // mapea la posición vertical del ratón (mouseY) a la frecuencia de la modulante entre 0 y 20 Hz
   var modFreq = map(mouseY, 0, height, 20, 0);
   modulator.freq(modFreq);
 
   var modAmp = map(mouseX, 0, width, 0, 1);
-  modulator.amp(modAmp, 0.01); // fade time of 0.1 for smooth fading
+  modulator.amp(modAmp, 0.01); // tiempo de transición de 0.1 para mayor suavidad
 
-  // analyze the waveform
+  // analiza la forma de onda
   waveform = fft.waveform();
 
-  // draw the shape of the waveform
+  // dibuja la forma de onda
   drawWaveform();
 
   drawText(modFreq, modAmp);
