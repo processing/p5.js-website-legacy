4327a0ef401f104df85f5042b62a1fb5de5e1bea
aaron <aamontoya@gmail.com>
2016-08-16 21:21:19 -0400

    add spanish translation of sound examples

--- src/data/examples/es/33_Sound/14_Filter_LowPass.js
+++ src/data/examples/es/33_Sound/14_Filter_LowPass.js
@@ -1,13 +1,13 @@
 /**
- *  @name  Filter LowPass
- *  @description Apply a p5.LowPass filter to a p5.SoundFile.
- *  Visualize the sound with FFT.
- *  Map mouseX to the the filter's cutoff frequency
- *  and mouseY to resonance/width of the a BandPass filter
+ *  @name  Filtro pasabajos
+ *  @description Aplica un filtro pasabajos (p5.LowPass) a un archivo de audio (p5.SoundFile).
+ *  Visualiza el sonido con FFT.
+ *  Mapea la posición horizontal del ratón (mouseX) a la frecuencia de corte del filtro
+ *  y la vertical (mouseY) a la razón resonancia/ancho de un filtro pasabanda.
  *
- * <p><em><span class="small"> To run this example locally, you will need the
- * <a href="http://p5js.org/reference/#/libraries/p5.sound">p5.sound library</a>
- * a sound file, and a running <a href="https://github.com/processing/p5.js/wiki/Local-server">local server</a>.</span></em></p>
+ * <br><br><em><span class="small"> Para correr localmente este ejemplo, necesitarás la
+ * <a href="http://p5js.org/reference/#/libraries/p5.sound">biblioteca p5.sound</a>
+ * un archivo de audio y correr un <a href="https://github.com/processing/p5.js/wiki/Local-server">servidor local</a>.</span></em>
  */
 var soundFile;
 var fft;
@@ -23,13 +23,13 @@ function setup() {
   createCanvas(710, 256);
   fill(255, 40, 255);
 
-  // loop the sound file
+  // reproduce el archivo de sonido en bucle
   soundFile.loop();
 
   filter = new p5.LowPass();
 
-  // Disconnect soundfile from master output.
-  // Then, connect it to the filter, so that we only hear the filtered sound
+  // desconecta el archivo de audio de la salida maestra.
+  // luego, conéctalo al filtro, para que solo escuchemos el sonido filtrado
   soundFile.disconnect();
   soundFile.connect(filter);
 
@@ -39,19 +39,19 @@ function setup() {
 function draw() {
   background(30);
 
-  // Map mouseX to a the cutoff frequency from the lowest
-  // frequency (10Hz) to the highest (22050Hz) that humans can hear
+  // Mapea la posición horizontal del ratón (mouseX) a la frecuencia de corte desde
+  // la frecuencia más grave (10Hz) a la más aguda (22050Hz) que los humanos escuchan
   filterFreq = map (mouseX, 0, width, 10, 22050);
 
-  // Map mouseY to resonance (volume boost) at the cutoff frequency
+  // Mapea la posición vertical del ratón (mouseY) a la resonancia (aumento de volumen) en la frecuencia de corte
   filterRes = map(mouseY, 0, height, 15, 5);
 
-  // set filter parameters
+  // define los parámetros del filtro
   filter.set(filterFreq, filterRes);
 
-  // Draw every value in the FFT spectrum analysis where
-  // x = lowest (10Hz) to highest (22050Hz) frequencies,
-  // h = energy (amplitude / volume) at that frequency
+  // dibuja cada lugar en el análisis de espectro FFT donde
+  // x = frecuencia más grave (10Hz) a la más aguda (22050Hz),
+  // h = energía (amplitud / volumen) en esa frecuencia
   var spectrum = fft.analyze();
   noStroke();
   for (var i = 0; i< spectrum.length; i++){
