d9497de6f5b903564611389f00e97a3716bc4d32
Darius Morawiec <darius.morawiec@nok.onl>
2016-08-31 03:50:43 +0200

    Remove duplicate jQuery code

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -19,29 +19,3 @@ function closeSearch() {
   $('#search_field').css('visibility', 'hidden');
   console.log($('#search_field').attr('width'));
 }
-
-$(window).ready(function() {
-  if (window.location.pathname.indexOf('/es/') === -1) {
-    $('#en-btn').attr('disabled', true);
-    $('#es-btn').attr('disabled', false);
-  } else {
-    $('#en-btn').attr('disabled', false);
-    $('#es-btn').attr('disabled', true);
-  }
-
-  $('#i18n-btn button').click(function() {
-    var loc = String(window.location.pathname);
-    loc = loc.replace('\/es\/', '');
-    var id = $(this).attr('id');
-    console.log(id)
-    if (id !== 'en-btn') {
-      loc = '/'+ id.substring(0, 2) + loc;
-    } else {
-      loc = '/' + loc;
-    }
-    console.log(loc)
-    window.location = loc;
-  });
-});
-
-
