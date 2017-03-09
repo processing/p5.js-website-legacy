26cc907e9cc40abd720c7d55a8bc8cda849d7322
Lauren McCarthy <laurmccarthy@gmail.com>
2017-03-09 13:25:40 -0800

    home

--- src/assets/p5_featured/ChellyJin_HaolinFang_MalalaWomensDay/sketch.js
+++ src/assets/p5_featured/ChellyJin_HaolinFang_MalalaWomensDay/sketch.js
@@ -53,9 +53,7 @@ function draw() {
     fill(246,144,101);
 
     console.log(window.location.hostname)
-    if (window.location.hostname.indexOf('diversity') == -1) {
-      fill('#68c7b7');
-    }
+    fill('#68c7b7');
     text("Click the screen to change image.", windowWidth*0.5, 100);
   }
   
