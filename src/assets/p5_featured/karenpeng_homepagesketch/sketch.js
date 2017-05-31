6e03ff9f464cffa551d43d6dd07cc74d3a5d3ea0
Chelly Jin <chellyjin@ucla.edu>
2017-05-30 21:39:23 -0700

    Update Karen Peng new homepage sketch

--- src/assets/p5_featured/karenpeng_homepagesketch/sketch.js
+++ src/assets/p5_featured/karenpeng_homepagesketch/sketch.js
@@ -1,7 +1,7 @@
 var FRAME_RATE = 50;
 var MY_RADIUS = 10;
 var MAX_SPEED = 10;
-var walkerManager;
+var walkerManagers = [];
 
 function Walker(x, y, radius, index) {
   this.x = x;
@@ -11,8 +11,8 @@ function Walker(x, y, radius, index) {
 }
 
 Walker.prototype.update = function(time) {
-  this.x += (sin(this.index + time) + map(noise(time, this.index), 0, 1, -1, 1));
-  this.y += (cos(this.index + time) + map(noise(time, this.index), 0, 1, -1, 1));
+  this.x += (sin(this.index + time) + map(noise(time, this.index), 0, 1, -2, 2));
+  this.y += (cos(this.index + time) + map(noise(time, this.index), 0, 1, -2, 2));
   this.radius -= 0.08;
 }
 
@@ -28,6 +28,8 @@ function WalkerManager(x, y) {
   this.x = x;
   this.y = y;
   this.loc = createVector(random(x), random(y));
+  this.loc.x = constrain(this.loc.x, MY_RADIUS, width - MY_RADIUS);
+  this.loc.y = constrain(this.loc.y, MY_RADIUS, height - MY_RADIUS);
   this.noff = createVector(random(10), random(10));
   this.counter = 0;
   this.history = [];
@@ -50,7 +52,7 @@ WalkerManager.prototype.walk = function (time) {
     this.noff.add(nLerp, nLerp);
   }
 
-  this.radius = noise(time) * 30;
+  this.radius = noise(time) * 40;
   this.history.push(new Walker(this.loc.x, this.loc.y, this.radius, this.counter));
   this.counter++;
 };
@@ -72,8 +74,8 @@ WalkerManager.prototype.render = function () {
     walker.render();
   });
   var lastWalker = this.history[this.history.length - 1];
-  fill(206, 33, 89, 90);
   var radius = Math.min(lastWalker.radius + 6, MY_RADIUS * 2);
+  fill(206, 33, 89, 90);
   ellipse(lastWalker.x, lastWalker.y, radius, radius);
 };
 
@@ -81,12 +83,16 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(FRAME_RATE);
   background(255);
-  walkerManager = new WalkerManager(width, height);
+  for(var i = 0; i < 1; i++) {
+    walkerManagers.push(new WalkerManager(width, height));
+  }
 }
 
 function draw() {
   background(255);
-  walkerManager.walk(frameCount * 0.1);
-  walkerManager.render();
-  walkerManager.update(frameCount * 0.1);
+  walkerManagers.forEach(function(walkerManager) {
+    walkerManager.walk(frameCount * 0.1);
+    walkerManager.render();
+    walkerManager.update(frameCount * 0.1);
+  });
 }
