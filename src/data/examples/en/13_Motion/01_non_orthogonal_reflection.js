ec7dd12689b3a7c8ef8bf526f2dc0469cffc5474
David Mickisch <dmickisch@gmail.com>
2018-05-20 16:43:29 +0200

    Simplify non-orthogonal-reflection example

--- src/data/examples/en/13_Motion/01_non_orthogonal_reflection.js
+++ src/data/examples/en/13_Motion/01_non_orthogonal_reflection.js
@@ -1,21 +1,19 @@
 /*
  * @name Non Orthogonal Reflection
- * @frame 710,400
+ * @frame 710,400 (optional)
  * @description This is a port by David Blitz of the "Reflection 1" example from processing.org/examples
  */
 
 //Position of left hand side of floor
 var base1
-  //Position of right hand side of floor
-  var base2;
+
+//Position of right hand side of floor
+var base2;
 //Length of floor
-var baseLength;
+//var baseLength;
 
-// An array of subpoints along the floor path
-var coords = []
-
-  // Variables related to moving ball
-  var position;
+// Variables related to moving ball
+var position;
 var velocity;
 var r = 6;
 var speed = 3.5;
@@ -26,12 +24,12 @@ function setup() {
   fill(128);
   base1 = createVector(0, height-150);
   base2 = createVector(width, height);
-  createGround();
+  //createGround();
 
-  // start ellipse at middle top of screen
+  //start ellipse at middle top of screen
   position = createVector(width/2, 0);
 
-  // calculate initial random velocity
+  //calculate initial random velocity
   velocity = p5.Vector.random2D();
   velocity.mult(speed);
 }
@@ -46,13 +44,14 @@ function draw() {
   fill(200);
   quad(base1.x, base1.y, base2.x, base2.y, base2.x, height, 0, height);
 
-  // calculate base top normal
+  //calculate base top normal
   var baseDelta = p5.Vector.sub(base2, base1);
   baseDelta.normalize();
   var normal = createVector(-baseDelta.y, baseDelta.x)
+  var intercept = p5.Vector.dot(base1, normal);
 
-    //draw ellipse
-    noStroke();
+  //draw ellipse
+  noStroke();
   fill(255);
   ellipse(position.x, position.y, r*2, r*2);
 
@@ -63,27 +62,25 @@ function draw() {
   incidence = p5.Vector.mult(velocity, -1);
   incidence.normalize();
 
-  // detect and handle collision
-  for ( var i=0; i<coords.length; ++i) {
-    // check distance between ellipse and base top coordinates
-    if ( p5.Vector.dist(position, coords[i]) < r) {
-      //calculate dot product of incident vector and base top 
-      var dot = incidence.dot(normal);
-      
-      //calculate reflection vector
-      //assign reflection vector to direction vector
-      velocity.set(2*normal.x*dot - incidence.x, 2*normal.y*dot - incidence.y, 0);
-      velocity.mult(speed);
-      
-      // draw base top normal at collision point
-      stroke(255, 128, 0);
-      line(position.x, position.y, position.x - normal.x*100, position.y - normal.y * 100);
-    }
+  // detect and handle collision with base
+  if (p5.Vector.dot(normal, position) > intercept) {
+    //calculate dot product of incident vector and base top 
+    var dot = incidence.dot(normal);
+
+    //calculate reflection vector
+    //assign reflection vector to direction vector
+    velocity.set(2*normal.x*dot - incidence.x, 2*normal.y*dot - incidence.y, 0);
+    velocity.mult(speed);
+
+    // draw base top normal at collision point
+    stroke(255, 128, 0);
+    line(position.x, position.y, position.x - normal.x*100, position.y - normal.y * 100);
   }
-  
+  //}
+
   // detect boundary collision
   // right
-  if(position.x > width - r) {
+  if (position.x > width - r) {
     position.x = width - r;
     velocity.x *= -1;
   }
@@ -93,29 +90,12 @@ function draw() {
     velocity.x *= -1;
   }
   // top
-  if( position.y < r ) {
-   position.y = r;
-   velocity.y *= -1;
-   
-   //randomize base top
-   
-   base1.y = random(height - 100, height);
-   base2.y = random(height - 100, height);
-   createGround();
-  }
-}
+  if ( position.y < r ) {
+    position.y = r;
+    velocity.y *= -1;
 
-// calculate variables for the ground
-var createGround = function() {
-  // calculate lenght of base top
-  baseLength = p5.Vector.dist(base1, base2);
-
-  // fill base top coordinate array
-  coords = [];
-  for (var i=0; i<ceil(baseLength); ++i) {
-    newCoord = createVector();
-    newCoord.x = base1.x + ((base2.x - base1.x) / baseLength) * i;
-    newCoord.y = base1.y + ((base2.y - base1.y) / baseLength) * i;
-    append(coords, newCoord);
+    //randomize base top
+    base1.y = random(height - 100, height);
+    base2.y = random(height - 100, height);
   }
 }
