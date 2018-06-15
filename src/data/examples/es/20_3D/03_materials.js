dbfa35be1c351dce93ae2a00783cb95652e69b1c
AidanNelson <AidanNelson@users.noreply.github.com>
2018-06-15 11:41:04 -0400

    update 3d materials & multiple lights examples in /es and /zh-Hans folders

--- src/data/examples/es/20_3D/03_materials.js
+++ src/data/examples/es/20_3D/03_materials.js
@@ -4,63 +4,62 @@
  * Reaccionan de distintas formas a la luz.
  * Mueve tu ratón para cambiar la posición de la luz.
  */
- 
 var img;
-function setup(){
+function setup() {
   createCanvas(710, 400, WEBGL);
   img = loadImage("assets/cat.jpg");
 }
 
-function draw(){
+function draw() {
   background(0);
 
-  var locY = (mouseY / height - 0.5) * (-2);
-  var locX = (mouseX / width - 0.5) * 2;
+  var locX = mouseX - height / 2;
+  var locY = mouseY - width / 2;
 
-  ambientLight(100, 80, 80);
-  pointLight(200, 200, 200, locX, locY, 0);
+  ambientLight(60, 60, 60);
+  pointLight(255, 255, 255, locX, locY, 100);
 
   push();
-  rotateZ(frameCount * 0.02);
-  rotateX(frameCount * 0.02);
-  rotateY(frameCount * 0.02);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
   texture(img);
   box(80);
   pop();
 
-  translate(-200, -250, 0);
   push();
-  rotateZ(frameCount * 0.02);
-  rotateX(frameCount * 0.02);
-  rotateY(frameCount * 0.02);
+  translate(-width / 4, -height / 4, 0);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
   fill(250, 0, 0);
   torus(80, 20, 64, 64);
   pop();
 
-  translate(400, 0, 0);
   push();
-  rotateZ(frameCount * 0.02);
-  rotateX(frameCount * 0.02);
-  rotateY(frameCount * 0.02);
+  translate(width / 4, -height / 4, 0);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
   normalMaterial();
   torus(80, 20, 64, 64);
   pop();
 
-  translate(-400, 500, 0);
   push();
-  rotateZ(frameCount * 0.02);
-  rotateX(frameCount * 0.02);
-  rotateY(frameCount * 0.02);
+  translate(-width / 4, height / 4, 0);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
   ambientMaterial(250);
   torus(80, 20, 64, 64);
   pop();
 
-  translate(400, 0, 0);
   push();
+  translate(width / 4, height / 4, 0);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
   specularMaterial(250);
-  rotateZ(frameCount * 0.02);
-  rotateX(frameCount * 0.02);
-  rotateY(frameCount * 0.02);
   torus(80, 20, 64, 64);
   pop();
 }
