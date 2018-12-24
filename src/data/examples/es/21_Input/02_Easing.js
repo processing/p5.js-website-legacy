31165893c64f409decd9aa9bb52ae176cbc89080
Animesh Sinha <animeshsinha1309@gmail.com>
2018-12-24 23:53:13 +0530

    Ported to ES6 classes
    Converted to ES6 syntax and fixed ESlint errors.

--- src/data/examples/es/21_Input/02_Easing.js
+++ src/data/examples/es/21_Input/02_Easing.js
@@ -1,32 +1,30 @@
 /*
  * @name Easing
  * @description Move the mouse across the screen and the symbol
- * will follow. Between drawing each frame of the animation, the 
+ * will follow. Between drawing each frame of the animation, the
  * program calculates the difference between the position of the
  * symbol and the cursor. If the distance is larger than 1 pixel,
  * the symbol moves part of the distance (0.05) from its current
  * position toward the cursor.
  */
-var x = 1;
-var y = 1;
-var easing = 0.05;
+let x = 1;
+let y = 1;
+let easing = 0.05;
 
 function setup() {
   createCanvas(720, 400);
   noStroke();
 }
 
-function draw()
-{
-    background(237,34,93);
-    var targetX = mouseX;
-    var dx = targetX - x;
-    x += dx * easing;
+function draw() {
+  background(237, 34, 93);
+  let targetX = mouseX;
+  let dx = targetX - x;
+  x += dx * easing;
 
-    var targetY = mouseY;
-    var dy = targetY - y;
-    y += dy * easing;
+  let targetY = mouseY;
+  let dy = targetY - y;
+  y += dy * easing;
 
-    ellipse(x,y,66,66);
-
+  ellipse(x, y, 66, 66);
 }
--- src/data/examples/es/21_Input/04_Mouse1D.js
+++ src/data/examples/es/21_Input/04_Mouse1D.js
@@ -13,12 +13,12 @@ function setup() {
 function draw() {
   background(230);
 
-  var r1 = map(mouseX, 0, width, 0, height);
-  var r2 = height - r1;
-  
-  fill(237,34,93,r1);
-  rect(width/2 + r1/2, height/2, r1, r1);
-  
-  fill(237,34,93,r2);
-  rect(width/2 - r2/2, height/2, r2, r2);
+  let r1 = map(mouseX, 0, width, 0, height);
+  let r2 = height - r1;
+
+  fill(237, 34, 93, r1);
+  rect(width / 2 + r1 / 2, height / 2, r1, r1);
+
+  fill(237, 34, 93, r2);
+  rect(width / 2 - r2 / 2, height / 2, r2, r2);
 }
