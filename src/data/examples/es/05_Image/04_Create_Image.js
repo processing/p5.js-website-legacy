16cfd3f68ef25680277482ec863f7c952e348a02
Animesh Sinha <animeshsinha1309@gmail.com>
2018-12-24 23:21:31 +0530

    Ported to ES6 classes
    Converted to ES6 syntax and fixed ESlint errors.

--- src/data/examples/es/05_Image/04_Create_Image.js
+++ src/data/examples/es/05_Image/04_Create_Image.js
@@ -3,15 +3,15 @@
  * @description La función createImage() provee un buffer fresco de pixeles para experimentar.
  * Este ejemplo crea un gradiente de imagen.
  */
-var img;  // Declarar variable 'img'.
+let img; // Declarar variable 'img'.
 
 function setup() {
   createCanvas(720, 400);
   img = createImage(230, 230);
   img.loadPixels();
-  for(var x = 0; x < img.width; x++) {
-    for(var y = 0; y < img.height; y++) {
-      var a = map(y, 0, img.height, 255, 0);
+  for (let x = 0; x < img.width; x++) {
+    for (let y = 0; y < img.height; y++) {
+      let a = map(y, 0, img.height, 255, 0);
       img.set(x, y, [0, 153, 204, a]);
     }
   }
@@ -21,5 +21,5 @@ function setup() {
 function draw() {
   background(0);
   image(img, 90, 80);
-  image(img, mouseX-img.width/2, mouseY-img.height/2);
+  image(img, mouseX - img.width / 2, mouseY - img.height / 2);
 }
