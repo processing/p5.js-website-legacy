6779fd153fc9105319b6dd5eb567f805ffebbc75
Animesh Sinha <animeshsinha1309@gmail.com>
2018-12-24 22:56:24 +0530

    Ported to ES6 classes
    Converted to ES6 syntax and fixed ESlint errors.

--- src/data/examples/en/02_Data/01_True_and_False.js
+++ src/data/examples/en/02_Data/01_True_and_False.js
@@ -1,37 +1,31 @@
 /*
  * @name True and False
- * @description A Boolean variable has only two possible values: true or false. 
- * It is common to use Booleans with control statements to determine the flow 
- * of a program. In this example, when the boolean value "b" is true, vertical 
- * lines are drawn and when the boolean value "b" is false, horizontal 
+ * @description A Boolean variable has only two possible values: true or false.
+ * It is common to use Booleans with control statements to determine the flow
+ * of a program. In this example, when the boolean value "b" is true, vertical
+ * lines are drawn and when the boolean value "b" is false, horizontal
  * lines are drawn.
  */
 function setup() {
-
   createCanvas(720, 400);
   background(0);
   stroke(255);
 
-  var b = false;
-  var d = 20;
-  var middle = width/2;
+  let b = false;
+  let d = 20;
+  let middle = width / 2;
 
-  for (var i = d; i <= width; i += d) {
-    
-    if (i < middle) {
-      b = true;
-    } else {
-      b = false;
-    }
-    
-    if (b == true) {
+  for (let i = d; i <= width; i += d) {
+    b = i < middle;
+
+    if (b === true) {
       // Vertical line
-      line(i, d, i, height-d);
+      line(i, d, i, height - d);
     }
-    
-    if (b == false) {
+
+    if (b === false) {
       // Horizontal line
-      line(middle, i - middle + d, width-d, i - middle + d);
+      line(middle, i - middle + d, width - d, i - middle + d);
     }
   }
 }
