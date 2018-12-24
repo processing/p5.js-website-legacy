7060b2b2598d353038f1b86a1532833b0c41632a
Animesh Sinha <animeshsinha1309@gmail.com>
2018-12-25 00:21:42 +0530

    Ported to ES6 classes
    Converted to ES6 syntax and fixed ESlint errors.

--- src/data/examples/zh-Hans/15_Instance_Mode/01_Instantiating.js
+++ src/data/examples/zh-Hans/15_Instance_Mode/01_Instantiating.js
@@ -3,10 +3,9 @@
  * @description Create a p5 instance, which keeps all variables
  * out of the global scope of your page.
  */
-var sketch = function( p ) {
-
-  var x = 100; 
-  var y = 100;
+let sketch = function(p) {
+  let x = 100;
+  let y = 100;
 
   p.setup = function() {
     p.createCanvas(700, 410);
@@ -15,15 +14,15 @@ var sketch = function( p ) {
   p.draw = function() {
     p.background(0);
     p.fill(255);
-    p.rect(x,y,50,50);
+    p.rect(x, y, 50, 50);
   };
 };
 
-var myp5 = new p5(sketch);
+let myp5 = new p5(sketch);
 
 // Compare to "global mode"
-// var x = 100;
-// var y = 100;
+// let x = 100;
+// let y = 100;
 
 // function setup() {
 //   createCanvas(200,200);
