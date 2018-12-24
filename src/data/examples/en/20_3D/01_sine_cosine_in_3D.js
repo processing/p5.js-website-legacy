03339cde1310065fd0851de0ddc11ffb409c37df
Animesh Sinha <animeshsinha1309@gmail.com>
2018-12-24 23:51:28 +0530

    Ported to ES6 classes
    Converted to ES6 syntax and fixed ESlint errors.

--- src/data/examples/en/20_3D/01_sine_cosine_in_3D.js
+++ src/data/examples/en/20_3D/01_sine_cosine_in_3D.js
@@ -2,21 +2,25 @@
  * @name Sine Cosine in 3D
  * @description Sine, cosine and push / pop could be applied in 3D as well.
  */
-function setup(){
+function setup() {
   createCanvas(710, 400, WEBGL);
 }
 
-function draw(){
+function draw() {
   background(250);
   rotateY(frameCount * 0.01);
 
-  for(var j = 0; j < 5; j++){
+  for (let j = 0; j < 5; j++) {
     push();
-    for(var i = 0; i < 80; i++){
-      translate(sin(frameCount * 0.001 + j) * 100, sin(frameCount * 0.001 + j) * 100, i * 0.1);
+    for (let i = 0; i < 80; i++) {
+      translate(
+        sin(frameCount * 0.001 + j) * 100,
+        sin(frameCount * 0.001 + j) * 100,
+        i * 0.1
+      );
       rotateZ(frameCount * 0.002);
       push();
-      sphere(8, 6, 4); 
+      sphere(8, 6, 4);
       pop();
     }
     pop();
--- src/data/examples/en/20_3D/07_orbit_control.js
+++ src/data/examples/en/20_3D/07_orbit_control.js
@@ -2,28 +2,32 @@
  * @name Orbit Control
  * @description Orbit control allows you to drag and move around the world.
  */
-function setup(){
+function setup() {
   createCanvas(710, 400, WEBGL);
 }
 
-function draw(){
+function draw() {
   background(250);
-  var radius = width * 1.5;
-  
+  let radius = width * 1.5;
+
   //drag to move the world.
   orbitControl();
 
   normalMaterial();
   translate(0, 0, -600);
-  for(var i = 0; i <= 12; i++){
-    for(var j = 0; j <= 12; j++){
+  for (let i = 0; i <= 12; i++) {
+    for (let j = 0; j <= 12; j++) {
       push();
-      var a = j/12 * PI;
-      var b = i/12 * PI;
-      translate(sin(2 * a) * radius * sin(b), cos(b) * radius / 2 , cos(2 * a) * radius * sin(b));    
-      if(j%2 === 0){
+      let a = (j / 12) * PI;
+      let b = (i / 12) * PI;
+      translate(
+        sin(2 * a) * radius * sin(b),
+        (cos(b) * radius) / 2,
+        cos(2 * a) * radius * sin(b)
+      );
+      if (j % 2 === 0) {
         cone(30, 30);
-      }else{
+      } else {
         box(30, 30, 30);
       }
       pop();
