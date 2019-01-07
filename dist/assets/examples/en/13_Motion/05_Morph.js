6bc7d20c87ce06fc7f27735dc09f49503a2e6d90
Gaurang Tandon <1gaurangtandon@gmail.com>
2019-01-07 18:07:21 +0530

    fixes #369

--- dist/assets/examples/en/13_Motion/05_Morph.js
+++ dist/assets/examples/en/13_Motion/05_Morph.js
@@ -7,24 +7,24 @@
 // Two ArrayLists to store the vertices for two shapes
 // This example assumes that each shape will have the same
 // number of vertices, i.e. the size of each ArrayList will be the same
-var circle = [];
-var square = [];
+let circle = [];
+let square = [];
 
 // An ArrayList for a third set of vertices, the ones we will be drawing
 // in the window
-var morph = [];
+let morph = [];
 
 // This boolean variable will control if we are morphing to a circle or square
-var state = false;
+let state = false;
 
 function setup() {
   createCanvas(720, 400);
 
   // Create a circle using vectors pointing from center
-  for (var angle = 0; angle < 360; angle += 9) {
+  for (let angle = 0; angle < 360; angle += 9) {
     // Note we are not starting from 0 in order to match the
     // path of a circle.
-    var v = p5.Vector.fromAngle(radians(angle - 135));
+    let v = p5.Vector.fromAngle(radians(angle - 135));
     v.mult(100);
     circle.push(v);
     // Let's fill out morph ArrayList with blank PVectors while we are at it
@@ -33,19 +33,19 @@ function setup() {
 
   // A square is a bunch of vertices along straight lines
   // Top of square
-  for (var x = -50; x < 50; x += 10) {
+  for (let x = -50; x < 50; x += 10) {
     square.push(createVector(x, -50));
   }
   // Right side
-  for (var y = -50; y < 50; y += 10) {
+  for (let y = -50; y < 50; y += 10) {
     square.push(createVector(50, y));
   }
   // Bottom
-  for (var x = 50; x > -50; x -= 10) {
+  for (let x = 50; x > -50; x -= 10) {
     square.push(createVector(x, 50));
   }
   // Left side
-  for (var y = 50; y > -50; y -= 10) {
+  for (let y = 50; y > -50; y -= 10) {
     square.push(createVector(-50, y));
   }
 }
@@ -54,11 +54,11 @@ function draw() {
   background(51);
 
   // We will keep how far the vertices are from their target
-  var totalDistance = 0;
+  let totalDistance = 0;
 
   // Look at each vertex
-  for (var i = 0; i < circle.length; i++) {
-    var v1;
+  for (let i = 0; i < circle.length; i++) {
+    let v1;
     // Are we lerping to the circle or square?
     if (state) {
       v1 = circle[i];
@@ -66,7 +66,7 @@ function draw() {
       v1 = square[i];
     }
     // Get the vertex we will draw
-    var v2 = morph[i];
+    let v2 = morph[i];
     // Lerp to the target
     v2.lerp(v1, 0.1);
     // Check how far we are from target
