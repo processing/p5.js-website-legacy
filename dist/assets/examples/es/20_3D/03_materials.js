81ec3a65f88f21e3d7e32231bb4323dd126cc35c
Lauren McCarthy <laurmccarthy@gmail.com>
2019-01-07 22:02:41 -0800

    rebuilding examples

--- dist/assets/examples/es/20_3D/03_materials.js
+++ dist/assets/examples/es/20_3D/03_materials.js
@@ -4,17 +4,17 @@
  * Reaccionan de distintas formas a la luz.
  * Mueve tu ratón para cambiar la posición de la luz.
  */
-var img;
+let img;
 function setup() {
   createCanvas(710, 400, WEBGL);
-  img = loadImage("assets/cat.jpg");
+  img = loadImage('assets/cat.jpg');
 }
 
 function draw() {
   background(0);
 
-  var locX = mouseX - height / 2;
-  var locY = mouseY - width / 2;
+  let locX = mouseX - height / 2;
+  let locY = mouseY - width / 2;
 
   ambientLight(60, 60, 60);
   pointLight(255, 255, 255, locX, locY, 100);
