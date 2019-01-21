af9bbfd72812c6e5ce56548d4367c67905fc6643
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-21 17:36:34 +0000

    Deploy processing/p5.js-website to github.com/processing/p5.js-website.git:gh-pages

--- assets/examples/en/00_Structure/01_Width_and_Height.js
+++ assets/examples/en/00_Structure/01_Width_and_Height.js
@@ -1,7 +1,7 @@
 /*
  * @name Width and Height
- * @description The 'width' and 'height' variables contain the 
- * width and height of the display window as defined in the createCanvas() 
+ * @description The 'width' and 'height' variables contain the
+ * width and height of the display window as defined in the createCanvas()
  * function.
  */
 function setup() {
@@ -11,7 +11,7 @@ function setup() {
 function draw() {
   background(127);
   noStroke();
-  for (var i = 0; i < height; i += 20) {
+  for (let i = 0; i < height; i += 20) {
     fill(129, 206, 15);
     rect(0, i, width, 10);
     fill(255);
--- assets/examples/es/00_Structure/02_Setup_and_Draw.js
+++ assets/examples/es/00_Structure/02_Setup_and_Draw.js
@@ -3,14 +3,14 @@
  * @description El código dentro de la función draw() corre continuamente de arriba
  * a a bajo hasta que el programa es parado.
  */
-var y = 100;
+let y = 100;
 
 // Las instrucciones dentro de la función setup()
 // se ejecutan una vez, al principio del programa
-function setup() ({
-	// createCanvas() debe ser la primera instrucción
+function setup() {
+  // createCanvas() debe ser la primera instrucción
   createCanvas(720, 400);
-  stroke(255);     // Hacer que el color de trazado sea blanco
+  stroke(255); // Hacer que el color de trazado sea blanco
   frameRate(30);
 }
 
@@ -19,7 +19,7 @@ function setup() ({
 // en orden y luego de que la última línea es leída,
 // se vuelve a ejecutar draw() desde el principio
 function draw() {
-  background(0);   // Hacer que el color del fondo sea negro
+  background(0); // Hacer que el color del fondo sea negro
   y = y - 1;
   if (y < 0) {
     y = height;
