ff0b0e5e56a31314be76b525cbb65e60f3b5e74d
17bcs029 <17bcs029@smvdu.ac.in>
2019-03-14 10:40:30 +0530

    add Inheritance example

--- src/data/examples/en/11_Objects/04_Inheritance.js
+++ src/data/examples/en/11_Objects/04_Inheritance.js
@@ -5,13 +5,14 @@
  * another is called a subclass, and the object it inherits from is called
  * a superclass. A subclass extends the superclass.
  */
-
 let spots, arm;
+
 function setup() {
   createCanvas(640, 360);
   arm = new SpinArm(width/2, height/2, 0.01);
   spots = new SpinSpots(width/2, height/2, -0.02, 90.0);
 }
+
 function draw() {
   background(204);
   arm.update();
@@ -33,24 +34,24 @@ class SpinArm {
   }
 
   display() {
-  strokeWeight(1);
-  stroke(0);
-  push();
-  translate(this.x, this.y);
-  this.angle += this.speed;
-  rotate(this.angle);
-  line(0, 0, 165, 0);
-  pop();
+    strokeWeight(1);
+    stroke(0);
+    push();
+    translate(this.x, this.y);
+    this.angle += this.speed;
+    rotate(this.angle);
+    line(0, 0, 165, 0);
+    pop();
   }
 }
 
 class SpinSpots {
   constructor(x, y, s, d) {
-	this.x = x;
-	this.y = y;
-	this.speed = s;
+	  this.x = x;
+	  this.y = y;
+	  this.speed = s;
     this.dim = d;
-	this.angle = 0.0;
+	  this.angle = 0.0;
   }
 
   update() {
@@ -58,13 +59,13 @@ class SpinSpots {
   }
 
   display() {
-  noStroke();
-  push();
-  translate(this.x, this.y);
-  this.angle += this.speed;
-  rotate(this.angle);
-  ellipse(-this.dim/2, 0, this.dim, this.dim);
-  ellipse(this.dim/2, 0, this.dim, this.dim);
-  pop();
+    noStroke();
+    push();
+    translate(this.x, this.y);
+    this.angle += this.speed;
+    rotate(this.angle);
+    ellipse(-this.dim/2, 0, this.dim, this.dim);
+    ellipse(this.dim/2, 0, this.dim, this.dim);
+    pop();
   }
 }
