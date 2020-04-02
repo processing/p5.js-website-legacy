5116d95ceeadff931eeb3b9a9ef68f3f9a40105c
Muying Li <38115784+MuyingLi@users.noreply.github.com>
2020-04-02 12:35:16 -0400

    translate the title and the word "mask" to "遮罩"

--- src/data/examples/zh-Hans/05_Image/03_Alpha_Mask.js
+++ src/data/examples/zh-Hans/05_Image/03_Alpha_Mask.js
@@ -1,7 +1,7 @@
 /*
- * @name Alpha Mask
- * @description 在图像上加载一个蒙版来改变图像中不同位置的透明度。
- * 通过拍p5.Image 中的mask() 函数来混合两张图像（图像和蒙版）。
+ * @name 透明度遮罩 (Alpha Mask)
+ * @description 在图像上加载一个遮罩来改变图像中不同位置的透明度。
+ * 通过拍p5.Image 中的mask() 函数来混合两张图像（图像和遮罩）。
  * <p><em><span class="small">要在本地运行此范例，您需要一个图像文件，并运行在<a href="https://github.com/processing/p5.js/wiki/Local-server">
  * 本地伺服器</a>上。</span></em></p>
  */
@@ -9,14 +9,14 @@ let img;
 let imgMask;
 
 function preload() {
-  // 加载图像及图像蒙版
+  // 加载图像及图像遮罩
   img = loadImage('assets/moonwalk.jpg');
   imgMask = loadImage('assets/mask.png');
 }
 
 function setup() {
   createCanvas(720, 400);
-  // mask() 函数将图像蒙版覆盖在图像上
+  // mask() 函数将图像遮罩覆盖在图像上
   img.mask(imgMask);
   imageMode(CENTER);
 }
