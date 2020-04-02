5d45bd1736d405f4e94a7bbb413cbfdd97d51559
MuyingLi <ml5333@nyu.edu>
2020-04-02 01:24:31 -0400

    translate all exmaples in section 05_Image into Chinese and all Chinese inline documentations

--- src/data/examples/zh-Hans/05_Image/03_Alpha_Mask.js
+++ src/data/examples/zh-Hans/05_Image/03_Alpha_Mask.js
@@ -1,22 +1,22 @@
 /*
  * @name Alpha Mask
- * @description Loads a "mask" for an image to specify the transparency in
- * different parts of the image. The two images are blended together using
- * the mask() method of p5.Image.
- * <p><em><span class="small"> To run this example locally, you will need two
- * image files, and a running <a href="https://github.com/processing/p5.js/wiki/Local-server">
- * local server</a>.</span></em></p>
+ * @description 在图像上加载一个蒙版来改变图像中不同位置的透明度。
+ * 通过拍p5.Image 中的mask() 函数来混合两张图像（图像和蒙版）。
+ * <p><em><span class="small">要在本地运行此范例，您需要一个图像文件，并运行在<a href="https://github.com/processing/p5.js/wiki/Local-server">
+ * 本地伺服器</a>上。</span></em></p>
  */
 let img;
 let imgMask;
 
 function preload() {
+  // 加载图像及图像蒙版
   img = loadImage('assets/moonwalk.jpg');
   imgMask = loadImage('assets/mask.png');
 }
 
 function setup() {
   createCanvas(720, 400);
+  // maske()函数将图像蒙版覆盖在图像上
   img.mask(imgMask);
   imageMode(CENTER);
 }
