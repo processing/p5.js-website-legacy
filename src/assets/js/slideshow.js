2f63768e1cf0b94fcdb1b27a5a0cb2aeb73dd6c5
Sarthak Saxena <saxenasarthak72@gmail.com>
2020-04-12 19:37:41 +0530

    Update slideshow.js

--- src/assets/js/slideshow.js
+++ src/assets/js/slideshow.js
@@ -2,12 +2,14 @@ var slideIndex = 0;
 carousel();
 
 function carousel() {
-  var x = $("#slideshow").children();
+  var x = $("#slideshow div");
   for (var i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; 
+    x[i].style.height = "0";
+    x[i].style.opacity = "0";
   }
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1} 
-  x[slideIndex-1].style.display = "block"; 
+  x[slideIndex-1].style.height = "400"; 
+  x[slideIndex-1].style.opacity = "1";
   setTimeout(carousel, 4000);
 }
