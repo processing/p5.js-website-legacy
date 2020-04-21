78df276d3099be8001f0f4348fa72e8c0f8ccd49
nedjulius <julius.nedzinskas@gmail.com>
2020-04-21 17:46:03 +0300

    added comments to the code

--- src/assets/js/cache-busting.js
+++ src/assets/js/cache-busting.js
@@ -1,6 +1,8 @@
 module.exports.register = function(Handlebars, options) {
   Handlebars.registerHelper('versionedAsset', function(options) {
+    // requiring crypto library for hashing
     const crypto = require('crypto');
+    // retrieving full path to the asset from the handlebar helper context
     const path = options.fn(this);
     const hash = crypto.createHash('sha256');
     let hashCode = "";
@@ -15,14 +17,21 @@ module.exports.register = function(Handlebars, options) {
   });
 };
 
+// function to map concatanated during the build files to their original files
+// and to return the concatenation string of them
 function concatFile(path) {
+  // mappings of concatanated files
   const routes = {
     "all.css": ["main.css", "prism.css", "normalize.css"]
   }
+  // including fs library to read from file
   const fs = require('fs');
+  // retrieving the asset that has to be read
   const fileName = path.split("/").pop();
+  // creating a full path to the asset
   const ext = __dirname.substr(0, __dirname.length - 2);
   let file = "";
+  // reading from all the mappings
   routes[fileName].forEach(f => {
     file += fs.readFileSync(ext + "css/" + f, function (err) { if (err) return 0 });
   })
