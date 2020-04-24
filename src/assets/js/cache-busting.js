017d28a347af6eb4bc0754f9cbf9e8b4e25604eb
nedjulius <julius.nedzinskas@gmail.com>
2020-04-25 01:07:42 +0300

    generalized script for any assets

--- src/assets/js/cache-busting.js
+++ src/assets/js/cache-busting.js
@@ -5,26 +5,16 @@ module.exports.register = function(Handlebars, options) {
     // retrieving full path to the asset from the handlebar helper context
     const path = options.fn(this);
     const hash = crypto.createHash('sha256');
+    const fs = require('fs');
+    const ext = __dirname.substr(0, __dirname.length - 3).replace("/src", "/dist");
     let hashCode = "";
     hash.on('readable', () => {
       const data = hash.read();
       if (data) hashCode = data.toString('hex');
     })
-    hash.write(readFromFile(path).toString());
+    hash.write(fs.readFileSync(ext + path, function (err) { if (err) return 0 }).toString());
     hash.end();
-    hashCode = path + "?v=" + hashCode.substr(0, 6);
+    hashCode = "/assets" + path + "?v=" + hashCode.substr(0, 6);
     return hashCode;
   });
 };
-
-// function that reads from file
-function readFromFile(path) {
-  // including fs library to read from file
-  const fs = require('fs');
-  // retrieving the asset that has to be read
-  const fileName = path.split("/").pop();
-  // creating a full path to the asset
-  const ext = __dirname.substr(0, __dirname.length - 2).replace("/src", "/dist");
-  console.log(ext + "css/" + fileName);
-  return fs.readFileSync(ext + "css/" + fileName, function (err) { if (err) return 0 });
-}
