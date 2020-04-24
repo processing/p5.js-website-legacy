269753c7c94d128f33956e1fed89cc9bf9eb05c5
nedjulius <julius.nedzinskas@gmail.com>
2020-04-24 16:28:49 +0300

    deleted unnecessary routing object

--- src/assets/js/cache-busting.js
+++ src/assets/js/cache-busting.js
@@ -10,30 +10,21 @@ module.exports.register = function(Handlebars, options) {
       const data = hash.read();
       if (data) hashCode = data.toString('hex');
     })
-    hash.write(concatFile(path).toString());
+    hash.write(readFromFile(path).toString());
     hash.end();
     hashCode = path + "?v=" + hashCode.substr(0, 6);
     return hashCode;
   });
 };
 
-// function to map concatanated during the build files to their original files
-// and to return the concatenation string of them
-function concatFile(path) {
-  // mappings of concatanated files
-  const routes = {
-    "all.css": ["main.css", "prism.css", "normalize.css"]
-  }
+// function that reads from file
+function readFromFile(path) {
   // including fs library to read from file
   const fs = require('fs');
   // retrieving the asset that has to be read
   const fileName = path.split("/").pop();
   // creating a full path to the asset
-  const ext = __dirname.substr(0, __dirname.length - 2);
-  let file = "";
-  // reading from all the mappings
-  routes[fileName].forEach(f => {
-    file += fs.readFileSync(ext + "css/" + f, function (err) { if (err) return 0 });
-  })
-  return file;
+  const ext = __dirname.substr(0, __dirname.length - 2).replace("/src", "/dist");
+  console.log(ext + "css/" + fileName);
+  return fs.readFileSync(ext + "css/" + fileName, function (err) { if (err) return 0 });
 }
