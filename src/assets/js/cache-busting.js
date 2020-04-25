4a349ed8ffe52d9c764e354f035812d335e0b5bc
nedjulius <julius.nedzinskas@gmail.com>
2020-04-25 14:06:18 +0300

    generalized path

--- src/assets/js/cache-busting.js
+++ src/assets/js/cache-busting.js
@@ -6,7 +6,7 @@ module.exports.register = function(Handlebars, options) {
     const path = options.fn(this);
     const hash = crypto.createHash('sha256');
     const fs = require('fs');
-    const ext = __dirname.substr(0, __dirname.length - 3).replace("/src", "/dist");
+    const ext = __dirname.substr(0, __dirname.length - 9).replace("/src", "/dist");
     let hashCode = "";
     hash.on('readable', () => {
       const data = hash.read();
@@ -14,7 +14,7 @@ module.exports.register = function(Handlebars, options) {
     })
     hash.write(fs.readFileSync(ext + path, function (err) { if (err) return 0 }).toString());
     hash.end();
-    hashCode = "/assets" + path + "?v=" + hashCode.substr(0, 6);
+    hashCode = "/" + path + "?v=" + hashCode.substr(0, 6);
     return hashCode;
   });
 };
