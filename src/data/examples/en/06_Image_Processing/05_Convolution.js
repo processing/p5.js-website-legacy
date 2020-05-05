0485bdaaf9e8eb73f52eb649a1d6252f6ae01b1b
Summer Rizzo <srainrizzo@gmail.com>
2020-05-05 16:52:31 -0400

    added convolution example

--- src/data/examples/en/06_Image_Processing/05_Convolution.js
+++ src/data/examples/en/06_Image_Processing/05_Convolution.js
@@ -1,13 +1,13 @@
 /*
  * @name Convolution
- * @description Applies a convolution matrix to a portion of an image. Move mouse to apply filter to different parts of the image. This example is a port of  <a href="https://processing.org/examples/convolution.html" target="blank">Dan Shiffman's example</a> for Processing. Original comments written by Dan.
+ * @description Applies a convolution matrix to a portion of an image. Move mouse to apply filter to different parts of the image. This example is a port of  <a href="https://processing.org/examples/convolution.html" target="blank">Dan Shiffman's example</a> for Processing. Original comments written by Dan unless otherwise specified.
  * <p><em><span class="small"> To run this example locally, you will need an
  * image file, and a running <a href="https://github.com/processing/p5.js/wiki/Local-server">
  * local server</a>.</span></em></p>
  */
  
 let img;
-let w = 120;
+let w = 80;
 
 // It's possible to convolve the image with many different 
 // matrices to produce different effects. This is a high-pass 
@@ -22,12 +22,15 @@ function preload() {
 
 function setup() {
   createCanvas(720, 400);
+  img.loadPixels();
+  // p5.js NOTE: in the case of retina displays, set the PD to 1
+  pixelDensity(1);
 }
 
 function draw() {
   // We're only going to process a portion of the image
   // so let's set the whole image as the background first
-  image(img, 0, 0);
+  background(img);
 
   // Calculate the small rectangle we will process
   const xstart = constrain(mouseX - w/2, 0, img.width);
@@ -37,35 +40,42 @@ function draw() {
   const matrixsize = 3;
 
   loadPixels();
-  
   // Begin our loop for every pixel in the smaller image
   for (let x = xstart; x < xend; x++) {
     for (let y = ystart; y < yend; y++ ) {
       let c = convolution(x, y, matrix, matrixsize, img);
-      let loc = x + y*img.width;
-      pixels[loc] = c;
+      // p5.js NOTE: The way the pixels array works in p5.js is slightly different than Processing. Instead of each pixel
+      // being a single value, their RGBA values are stored as seperate elements that we access individually.
+      let loc = (x + y*img.width) * 4;
+      pixels[loc] = red(c);
+      pixels[loc + 1] = green(c);
+      pixels[loc + 2] = blue(c);
+      pixels[loc + 3] = alpha(c);
     }
   }
   updatePixels();
 }
 
 function convolution(x, y, matrix, matrixsize, img) {
-  rtotal = 0.0;
-  gtotal = 0.0;
-  btotal = 0.0;
-  offset = matrixsize / 2;
-  for (i = 0; i < matrixsize; i++){
-    for (j= 0; j < matrixsize; j++){
+  let rtotal = 0.0;
+  let gtotal = 0.0;
+  let btotal = 0.0;
+  const offset = Math.floor(matrixsize / 2);
+  for (let i = 0; i < matrixsize; i++){
+    for (let j = 0; j < matrixsize; j++){
       // What pixel are we testing
-      xloc = x+i-offset;
-      yloc = y+j-offset;
-      loc = xloc + img.width*yloc;
+      const xloc = (x + i - offset);
+      const yloc = (y + j - offset);
+      let loc = (xloc + img.width * yloc) * 4;
+
       // Make sure we haven't walked off our image, we could do better here
-      loc = constrain(loc,0,img.pixels.length-1);
+      loc = constrain(loc, 0 , img.pixels.length - 1);
+
       // Calculate the convolution
-      rtotal += (red(img.pixels[loc]) * matrix[i][j]);
-      gtotal += (green(img.pixels[loc]) * matrix[i][j]);
-      btotal += (blue(img.pixels[loc]) * matrix[i][j]);
+      // p5.js NOTE: same thing is going on here - we must access the RGB values individually
+      rtotal += (img.pixels[loc]) * matrix[i][j];
+      gtotal += (img.pixels[loc + 1]) * matrix[i][j];
+      btotal += (img.pixels[loc + 2]) * matrix[i][j];
     }
   }
   // Make sure RGB is within range
