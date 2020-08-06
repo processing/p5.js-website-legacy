33409eb143a9b1980da71768e0300f0b2ff6739a
unknown <yinhwa.erica@gmail.com>
2020-08-07 00:04:25 +0900

    test designs

--- src/assets/js/teach.js
+++ src/assets/js/teach.js
@@ -1,3 +1,4 @@
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -12,3 +13,21 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// <script>
+
+//   var acc = document.getElementsByClassName("accordion");
+//   var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
+    
