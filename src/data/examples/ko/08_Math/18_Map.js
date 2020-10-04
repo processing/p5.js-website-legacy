846f8378fbfdc92786551fca28de4368f1593e1d
Berke Özgen <56646605+berkeozgen08@users.noreply.github.com>
2020-10-04 19:24:57 +0300

    Update 18_Map.js

--- src/data/examples/ko/08_Math/18_Map.js
+++ src/data/examples/ko/08_Math/18_Map.js
@@ -6,15 +6,15 @@
  * 이 예제에서는 마우스의 x 좌표(0과 360사이의 숫자)가 원형의 색상과 크기를 정의하는 새로운 숫자들로 처리됩니다.
  */
 function setup() {
-  createCanvas(640, 400);
+  createCanvas(720, 400);
   noStroke();
 }
 
 function draw() {
   background(0);
-  // 0부터 640에 이르는 mouseX 값을 0부터 175의 범위로 조정
+  // 0부터 720에 이르는 mouseX 값을 0부터 175의 범위로 조정
   let c = map(mouseX, 0, width, 0, 175);
-  // 0부터 640에 이르는 mouseX 값을 40부터 300의 범위로 조정
+  // 0부터 720에 이르는 mouseX 값을 40부터 300의 범위로 조정
   let d = map(mouseX, 0, width, 40, 300);
   fill(255, c, 0);
   ellipse(width/2, height/2, d, d);
