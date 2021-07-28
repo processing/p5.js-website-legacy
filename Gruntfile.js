683219d553806b637af789cc9deff025d033eb4b
limzykenneth <limzy.kenneth@gmail.com>
2021-07-28 01:36:31 +0100

    Add fluent-JSON conversion script and unfinished grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,10 +10,15 @@ const yaml = require('js-yaml');
 const fs = require('fs').promises;
 const fse = require('fs-extra');
 const git = require('simple-git');
+const _ = require('lodash');
 const pkg = require('./package.json');
 const update_i18n = require('./updatei18nFiles.js');
+const fluentConverter = require('./fluentConverter.js');
 const mozjpeg = require('imagemin-mozjpeg');
 const pngquant = require('imagemin-pngquant');
+const util = require('util');
+const glob = util.promisify(require('glob'));
+const path = require('path');
 
 module.exports = function(grunt) {
   require('time-grunt')(grunt);
@@ -412,6 +417,62 @@ module.exports = function(grunt) {
         YAMLfiles_URL + langCode + '.yml'
       );
     });
+  });
+
+  grunt.registerTask('json-to-fluent', async function(){
+    const done = this.async();
+    const languages = pkg.languages;
+    const promises = [];
+
+    try{
+      for(const language of languages){
+        const fileData = await fs.readFile(`./src/data/reference/${language}.json`);
+        const ftlStrs = fluentConverter.jsonToFtl(fileData);
+        fse.mkdirpSync(`./src/data/localization/${language}/`);
+        _.each(ftlStrs, (str, name) => {
+          promises.push(fs.writeFile(`./src/data/localization/${language}/${name}.ftl`, str));
+        });
+      }
+
+      await Promise.all(promises);
+      done();
+    }catch(err){
+      done(err);
+    }
+  });
+
+  grunt.registerTask('fluent-to-json', async function(){
+    const done = this.async();
+    const languages = pkg.languages;
+    const promises = [];
+
+    try{
+      for(const language of languages){
+        const fileData = await fs.readFile(`./src/data/reference/${language}.json`);
+        const data = JSON.parse(fileData);
+        const newData = _.cloneDeep(data);
+
+        const files = await glob(`./src/data/localization/${language}/*.ftl`);
+        for(const file of files) {
+          if(
+            file !== `./src/data/localization/${language}/root.ftl`
+          ){
+            const key = path.basename(file, '.ftl');
+            const fileData = await fs.readFile(file, {encoding: 'utf8'});
+            const jsonData = fluentConverter.ftlToObj(fileData);
+
+            _.assign(newData[key], jsonData);
+         }
+        }
+      }
+
+      // Write data out to JSON files
+      // Will be implemented when confirm there will be no data loss
+      console.log('File write skipped.');
+      done();
+    }catch(err){
+      done(err);
+    }
   });
 
   grunt.loadNpmTasks('grunt-exec');
