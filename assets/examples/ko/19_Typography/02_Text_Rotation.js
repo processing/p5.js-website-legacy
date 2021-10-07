a0eed191ecf009c0f21f7fe1acf4b8a79cd6098f
limzykenneth <limzy.kenneth@gmail.com>
2021-10-07 17:25:08 +0000

    Deploying to gh-pages from @ processing/p5.js-website@b8fa27b6e826e0011382571f3f0f68e6886b7669 🚀

--- assets/examples/ko/19_Typography/02_Text_Rotation.js
+++ assets/examples/ko/19_Typography/02_Text_Rotation.js
@@ -1,25 +1,25 @@
 /*
- * @name Text Rotation
- * @description Draws letters to the screen and rotates them at different angles.
- *  (ported from https://processing.org/examples/textrotation.html) 
+ * @name 텍스트 회전
+ * @description 스크린에 글자를 그린 후, 다양한 각도로 회전시킵니다.
+ * (https://processing.org/examples/textrotation.html에서 옮김)
  */
 
 let font,
-  fontsize = 32;
+fontsize = 32;
 
 let angleRotate = 0.0;
 
 function setup() {
   createCanvas(710, 400);
   background(0);
-  
-  // Ensure the .ttf or .otf font stored in the assets directory
-  // is loaded before setup() and draw() are called
+
+  // setup()함수와 draw()함수를 부르기 전에 .ttf 또는 .otf 파일이 'assets' 주소에
+  // 있을 것을 확안합니다
   font = loadFont('assets/SourceSansPro-Regular.otf');
-  
-  // Set text characteristics
+
+  // 텍스트의 특징을 정합니다
   textFont(font);
-} 
+}
 
 function draw() {
   background(0);
@@ -31,7 +31,7 @@ function draw() {
   let angle1 = radians(45);
   translate(100, 180);
   rotate(angle1);
-  // Draw the letter to the screen
+  // 스크린에 글짜를 그립니다
   text("45 DEGREES", 0, 0);
   line(0, 0, 150, 0);
   pop();
@@ -40,18 +40,18 @@ function draw() {
   let angle2 = radians(270);
   translate(200, 180);
   rotate(angle2);
-  // Draw the letter to the screen
+  // 스크린에 글짜를 그립니다
   text("270 DEGREES", 0, 0);
   line(0, 0, 150, 0);
   pop();
-  
+
   push();
   translate(440, 180);
   rotate(radians(angleRotate));
   text(int(angleRotate) % 360 + " DEGREES ", 0, 0);
   line(0, 0, 150, 0);
   pop();
-  
+
   angleRotate += 0.25;
 
   stroke(255, 0, 0);
