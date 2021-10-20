5b6da8b6a73562abfb8b311f203847adad43fdfa
Devesh <rockdev1234560@gmail.com>
2021-10-20 14:00:02 +0530

    Added .yml fixes and folder decorator fixes

--- src/data/examples/hi/23_Camera/02_perspective.js
+++ src/data/examples/hi/23_Camera/02_perspective.js
@@ -1,7 +1,7 @@
 /**
- * Perspective. 
+ * @name Perspective. 
  * 
- * Move the mouse left and right to change the field of view (fov).
+ * @description Move the mouse left and right to change the field of view (fov).
  * Click to modify the aspect ratio. The perspective() function
  * sets a perspective projection applying foreshortening, making 
  * distant objects appear smaller than closer ones. The parameters 
@@ -14,27 +14,27 @@
  * to set the area precisely.
  */
 
- void setup() {
-    size(640, 360, P3D);
-    noStroke();
-  }
-  
-  void draw() {
-    lights();
-    background(0);
-    float cameraY = height/2.0;
-    float fov = mouseX/float(width) * PI/2;
+void setup() {
+  size(640, 360, P3D);
+  noStroke();
+}
+
+void draw() {
+  lights();
+  background(0);
+    float cameraY = height / 2.0;
+    float fov = mouseX / float(width) * PI / 2;
     float cameraZ = cameraY / tan(fov / 2.0);
-    float aspect = float(width)/float(height);
-    if (mousePressed) {
-      aspect = aspect / 2.0;
-    }
-    perspective(fov, aspect, cameraZ/10.0, cameraZ*10.0);
-    
-    translate(width/2+30, height/2, 0);
-    rotateX(-PI/6);
-    rotateY(PI/3 + mouseY/float(height) * PI);
-    box(45);
-    translate(0, 0, -50);
-    box(30);
+    float aspect = float(width) / float(height);
+  if (mousePressed) {
+    aspect = aspect / 2.0;
   }
+  perspective(fov, aspect, cameraZ / 10.0, cameraZ * 10.0);
+
+  translate(width / 2 + 30, height / 2, 0);
+  rotateX(-PI / 6);
+  rotateY(PI / 3 + mouseY / float(height) * PI);
+  box(45);
+  translate(0, 0, -50);
+  box(30);
+}
