7d1a57291b5439f60679109649c7626a6c992600
limzykenneth <limzy.kenneth@gmail.com>
2022-03-18 15:12:35 +0000

    Deploying to gh-pages from @ processing/p5.js-website@e1523b40055f4a710fe659b34b5099705fa616fb 🚀

--- assets/examples/es/19_Typography/02_Text_Rotation.js
+++ assets/examples/es/19_Typography/02_Text_Rotation.js
@@ -1,7 +1,8 @@
 /*
  * @name Text Rotation
  * @description Draws letters to the screen and rotates them at different angles.
- *  (ported from https://processing.org/examples/textrotation.html) 
+ * <br><br><small><em>This example is ported from the <a href="https://processing.org/examples/textrotation.html">Text Rotation example</a>
+ * on the Processing website</em></small>
  */
 
 let font,
