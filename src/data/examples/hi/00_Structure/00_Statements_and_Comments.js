b8c40a784c3726104969a1191aa9bd3c2775f66d
akshatnema <20bcs022@iiitdmj.ac.in>
2022-03-19 21:15:46 +0530

    comments file added in Hindi

---
+++ src/data/examples/hi/00_Structure/00_Statements_and_Comments.js
@@ -0,0 +1,25 @@
+/*
+ * @name टिप्पणी & कथन
+ * @arialabel सरसों की पीली पृष्ठभूमि
+ * @description कथन वे तत्व हैं जो प्रोग्राम बनाते हैं। अर्धविराम का उपयोग अंतिम कथनों के लिए किया जाता है।
+ * इसे "स्टेटमेंट टर्मिनेटर" कहा जाता है। टिप्पणियों का उपयोग लोगों को कार्यक्रमों को बेहतर ढंग से समझने 
+ * में मदद करने के लिए नोट्स बनाने के लिए किया जाता है। एक टिप्पणी दो फॉरवर्ड स्लैश ("//") से शुरू होती  
+ * है।. (https://processing.org/examples/statementscomments.html से पोर्ट किया गया)
+ */
+
+// The createCanvas function is a statement that tells the computer 
+// how large to make the window.
+// Each function statement has zero or more parameters. 
+// Parameters are data passed into the function
+// and are used as values for telling the computer what to do.
+function setup() {
+    createCanvas(710, 400);
+ }
+ 
+ // The background function is a statement that tells the computer
+ // which color (or gray value) to make the background of the display window 
+ function draw() {
+    background(204, 153, 0);
+ }
+ 
+ 
