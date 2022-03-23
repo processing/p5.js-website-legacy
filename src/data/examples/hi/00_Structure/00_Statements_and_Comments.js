38d94f4ceec92b3275216342f58633673c7f67b1
Divyansh013 <divyansh.srivastava.cd.che20@itbhu.ac.in>
2022-03-23 23:11:33 +0530

    fixed bugs while opening example

---
+++ src/data/examples/hi/00_Structure/00_Statements_and_Comments.js
@@ -0,0 +1,24 @@
+/*
+ * @name कथन और टिप्पणियाँ
+ * @arialabel पीला पृष्ठभूमि
+ * @description कथन वे तत्व हैं जो कार्यक्रम बनाते हैं। ";" (सेमी-कोलन) चिन्ह का प्रयोग कथनों को समाप्त करने के लिए किया जाता है।
+ * इसे "स्टेटमेंट टर्मिनेटर" कहा जाता है। टिप्पणियों का उपयोग लोगों को कार्यक्रमों को बेहतर ढंग से समझने में मदद करने के लिए नोट्स बनाने के लिए किया जाता है। एक टिप्पणी दो फॉरवर्ड स्लैश ("//") से शुरू होती है।
+ * <br><br><small><em>यह उदाहरण <a href="https://processing.org/examples/statementscomments.html">बयानों और टिप्पणियों के उदाहरण</a> से लिया गया है
+ * संसाधन वेबसाइट पर</em></small>
+ */
+// The createCanvas function is a statement that tells the computer 
+// how large to make the window.
+// Each function statement has zero or more parameters. 
+// Parameters are data passed into the function
+// and are used as values for telling the computer what to do.
+function setup() {
+    createCanvas(710, 400);
+ }
+ 
+ // The background function is a statement that tells the computer
+ // which color (or gray value) to make the background of the display window 
+ function draw() {
+    background(204, 153, 0);
+ }
+ 
+ 
