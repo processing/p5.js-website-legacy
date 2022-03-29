6163f64ecf9d92b2f46ec934ca3a1a91ddbf4acd
limzykenneth <limzy.kenneth@gmail.com>
2022-03-29 14:00:02 +0000

    Deploying to gh-pages from @ processing/p5.js-website@e341b21b4307b82bc985772a6f5e9e1254d9772c 🚀

--- assets/examples/en/00_Structure/05_Loop.js
+++ assets/examples/en/00_Structure/05_Loop.js
@@ -1,10 +1,12 @@
 /*
  * @name Loop
  * @arialabel Horizontal white line on a black background that moves from the bottom to the top of the screen parallel to the x-axis
- * @description The code inside the draw() function runs continuously from top
- * to bottom until the program is stopped.
+ * @description If noLoop() is run in setup(), the code in draw() 
+ * is only run once. In this example, click the mouse 
+ * to run the loop() function to cause the draw() the 
+ * run continuously. 
  */
-let y = 100;
+let y = 0;
 
 // The statements in the setup() function
 // execute once when the program begins
@@ -12,6 +14,7 @@ function setup() {
   createCanvas(720, 400); // Size must be the first statement
   stroke(255); // Set line drawing color to white
   frameRate(30);
+  noLoop();
 }
 // The statements in draw() are executed until the
 // program is stopped. Each statement is executed in
@@ -24,4 +27,8 @@ function draw() {
     y = height;
   }
   line(0, y, width, y);
+}
+
+function mousePressed(){
+  loop();
 }
