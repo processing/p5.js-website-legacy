8037ece199e642ccbcc2c0221c6ee5c78d19fd74
Guirdo <produccionesaldo@gmail.com>
2022-11-22 10:21:18 -0600

    Update spanish variables names in 24_Wavemaker.js

--- src/data/examples/es/10_Interaction/24_Wavemaker.js
+++ src/data/examples/es/10_Interaction/24_Wavemaker.js
@@ -20,14 +20,14 @@ function draw() {
   for (let x = 0; x <= width; x = x + 30) {
     for (let y = 0; y <= height; y = y + 30) {
       // punto de partida de cada círculo depende de posición del ratón
-      const anguloX = map(mouseX, 0, width, -4 * PI, 4 * PI, true);
-      const anguloY = map(mouseY, 0, height, -4 * PI, 4 * PI, true);
+      const xAngle = map(mouseX, 0, width, -4 * PI, 4 * PI, true);
+      const yAngle = map(mouseY, 0, height, -4 * PI, 4 * PI, true);
       // y también varía según la posición de la partícula
-      const angulo = anguloX * (x / width) + anguloY * (y / height);
+      const angle = xAngle * (x / width) + yAngle * (y / height);
 
       // cada partícula se mueve en forma circular
-      const myX = x + 20 * cos(2 * PI * t + angulo);
-      const myY = y + 20 * sin(2 * PI * t + angulo);
+      const myX = x + 20 * cos(2 * PI * t + angle);
+      const myY = y + 20 * sin(2 * PI * t + angle);
 
       ellipse(myX, myY, 10); // dibujar partícula
     }
