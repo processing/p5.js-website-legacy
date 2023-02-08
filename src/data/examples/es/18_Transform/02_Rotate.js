1ad42519d9ecb51fc7c83cfa2eb3ac3daa4a3e94
Guirdo <produccionesaldo@gmail.com>
2023-02-07 22:38:12 -0600

    Update comments and variables in 02_Rotates

--- src/data/examples/es/18_Transform/02_Rotate.js
+++ src/data/examples/es/18_Transform/02_Rotate.js
@@ -1,29 +1,26 @@
 /*
  * @name Rotate
  * @description Rotar un cuadrado en torno al eje Z
- Si prefieres usar grados (0-360) para medir los ángulos, puede usas el
- * método radians() para convertir tus valores a radianes
- * For example: scale(radians(90)) is identical to the statement
- * scale(PI/2). In this example, every even numbered second a jitter
- * is added to the rotation. During odd seconds rotation moves CW and
- * CCW at the speed determined by the last jitter value.
  * Para obtener los resultados que esperas, usa ángulos de la función
  * rotate() (del inglés rotar) con valores entre 0 y PI*2 (TWO_PI que
  * es aproximadamente 6.28)
- *
- *
- *
+ * Si prefieres usar grados (0-360) para medir los ángulos, puede usar el
+ * método radians() para convertir tus valores a radianes
+ * Por ejemplo: rotate(radians(90)) es idéntico a la sentencia
+ * rotate(PI/2). En este ejemplo, cada segundo numerado un jitter
+ * es añadido a la rotación. Durante los segundos impares la rotación se mueve CW y
+ * CCW a la velocidad determinada por el ultimo valor de jitter.
  */
 
-let angulo = 0.0;
+let angle = 0.0;
 let jitter = 0.0;
 
 function setup() {
   createCanvas(720, 400);
   noStroke();
   fill(255);
-  // Dibuja el rectánglo desde el centro y también hará que
-  //la rotación sea en torno al centro
+  // Dibuja el rectángulo desde el centro y también hará que
+  // la rotación sea en torno al centro
   rectMode(CENTER);
 }
 
@@ -35,11 +32,11 @@ function draw() {
   if (second() % 2 === 0) {
     jitter = random(-0.1, 0.1);
   }
-  //increase the angle value using the most recent jitter value
-  angulo = angulo + jitter;
+  //incrementa el valor del ángulo usando el valor más reciente de jitter
+  angle = angle + jitter;
   // Usa coseno para obtener un movimiento suave a favor y en contra
   // de las manecillas del reloj cuando no esté  haciendo jittering
-  let c = cos(angulo);
+  let c = cos(angle);
   // Mueve la figura al centro del lienzo
   translate(width / 2, height / 2);
   // Aplica la rotación final
