8e0a1dc6011e7089305c20355fa529ec9cc07c70
Guirdo <produccionesaldo@gmail.com>
2023-02-08 14:09:07 -0600

    Update comments in 04_No_Loop

--- src/data/examples/es/00_Structure/04_No_Loop.js
+++ src/data/examples/es/00_Structure/04_No_Loop.js
@@ -1,6 +1,7 @@
 /*
  * @name No Loop
- * @description La función noLoop() hace que draw() se ejecuta solo una vez.
+ * @arialabel Linea horizontal de color blanco en medio de un fondo color negro
+ * @description La función noLoop() hace que draw() se ejecute solo una vez.
  * Sin ejecutar noLoop(), el código dentro de draw() es ejecutado continuamente.
  */
 let y;
@@ -10,14 +11,14 @@ let y;
 function setup() {
   // createCanvas deberá ser la primera sentencia
   createCanvas(720, 400);
-  stroke(255); // Pone al color blanco para dibujar lineas
+  stroke(255); // Hace que el color de trazado sea blanco
   noLoop();
 
   y = height * 0.5;
 }
 
 // Las instrucciones en draw() son ejecutadas hasta que
-// el programa es parado. Cada instrucción es ejecutada
+// el programa es detenido. Cada instrucción es ejecutada
 // en orden y luego de que la última línea es leída,
 // se vuelve a ejecutar draw() desde el principio
 function draw() {
