448f092151b444b1e10ad518110f3e88934811ae
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-04-28 20:54:11 +0900

    fix: fix indentation and format of source code for some examples/en/05_Image/.js files

--- src/data/examples/en/05_Image/06_Blur.js
+++ src/data/examples/en/05_Image/06_Blur.js
@@ -10,17 +10,21 @@
 // v is the normalized value
 let v = 1.0 / 9.0;
 // kernel is the 3x3 matrix of normalized values
-let kernel = [[ v, v, v ], [ v, v, v ], [ v, v, v ]]; 
+let kernel = [
+  [v, v, v],
+  [v, v, v],
+  [v, v, v]
+];
 
 // preload() runs once, before setup()
 // loadImage() needs to occur here instead of setup()
-// if loadImage() is called in setup(), the image won't appear 
+// if loadImage() is called in setup(), the image won't appear
 // since noLoop() restricts draw() to execute only once
 // (one execution of draw() is not enough time for the image to load),
 // preload() makes sure image is loaded before anything else occurs
 function preload() {
   // load the original image
-  img = loadImage("assets/rover.png"); 
+  img = loadImage('assets/rover.png');
 }
 
 // setup() runs once after preload
@@ -30,7 +34,6 @@ function setup() {
   // noLoop() makes draw() run only once, not in a loop
   noLoop();
 }
-
 
 // draw() runs after setup(), normally on a loop
 // in this case it runs only once, because of noDraw()
@@ -43,7 +46,7 @@ function draw() {
 
   // load its pixels
   edgeImg.loadPixels();
-  
+
   // two for() loops, to iterate in x axis and y axis
   // since the kernel assumes that the pixel
   // has pixels above, under, left, and right
@@ -53,7 +56,7 @@ function draw() {
   for (let x = 1; x < img.width; x++) {
     for (let y = 1; y < img.height; y++) {
       // kernel sum for the current pixel starts as 0
-      let sum = 0; 
+      let sum = 0;
 
       // kx, ky variables for iterating over the kernel
       // kx, ky have three different values: -1, 0, 1
@@ -61,10 +64,10 @@ function draw() {
         for (ky = -1; ky <= 1; ky++) {
           let xpos = x + kx;
           let ypos = y + ky;
-          
-          // since our image is grayscale, 
+
+          // since our image is grayscale,
           // RGB values are identical
-          // we retrieve the red value for this example 
+          // we retrieve the red value for this example
           // (green and blue work as well)
           let val = red(img.get(xpos, ypos));
 
@@ -74,7 +77,7 @@ function draw() {
           // if we add 1 to kx and ky, we get 0, 1, 2
           // with that we can use it to iterate over kernel
           // and calculate the accumulated sum
-          sum += kernel[kx+1][ky+1] * val;
+          sum += kernel[kx + 1][ky + 1] * val;
         }
       }
 
@@ -84,7 +87,7 @@ function draw() {
   }
   // updatePixels() to write the changes on edgeImg
   edgeImg.updatePixels();
-  
+
   // draw edgeImg at the right of the original image
   image(edgeImg, img.width, 0);
 }
