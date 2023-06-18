f6e6b5e276d4e8bb85bacb9eb9f282cb3c436a2a
shibomb <shibomb@gmail.com>
2023-06-19 02:38:17 +0900

    Fix :  Added missing description.
    Changes:
    Added description of unused saved function same as Load Saved JSON.

--- src/data/examples/en/22_Advanced_Data/01_Load_Saved_Table.js
+++ src/data/examples/en/22_Advanced_Data/01_Load_Saved_Table.js
@@ -4,7 +4,7 @@
  * @description Create a Bubble class, instantiate multiple bubbles using data from
  * a csv file, and display results on the screen.
  *  Because the web browsers differ in where they save files, we do not make use of
- * 
+ * saveTable, unlike the Processing example.<br><br>
  * Based on Daniel Shiffman's <a href="https://processing.org/examples/loadsavetable.html">LoadSaveTable Example</a> for Processing.
  */
 
